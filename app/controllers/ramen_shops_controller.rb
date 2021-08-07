f60d61ec113344e2ca94d20dc480bf4129a102b9
shunsaku0621 <hamagi.0621@icloud.com>
2021-08-07 22:21:24 +0900

    修正

--- app/controllers/ramen_shops_controller.rb
+++ app/controllers/ramen_shops_controller.rb
@@ -1,2 +1,4 @@
 class RamenShopsController < ApplicationController
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
   get 'homes/index'
+  resources :ramen_shops
 end
