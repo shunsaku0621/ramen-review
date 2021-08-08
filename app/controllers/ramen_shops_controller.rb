d4dd0767eab06f5ae036eae83967b2428f274033
shunsaku0621 <hamagi.0621@icloud.com>
2021-08-08 19:15:05 +0900

    保存機能

--- app/controllers/ramen_shops_controller.rb
+++ app/controllers/ramen_shops_controller.rb
@@ -4,5 +4,19 @@ class RamenShopsController < ApplicationController
   end
   
   def new
+    @ramen_shop = RamenShop.new
+  end
+
+  def create
+    RamenShop.create(ramen_params)
+    redirect_to ramen_shops_path
+  end
+
+
+
+
+  private
+  def ramen_params
+    params.require(:ramen_shop).permit(:name)
   end
 end
