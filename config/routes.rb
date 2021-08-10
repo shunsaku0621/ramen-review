d2df8e1f0d43eb6bf08d02e54e5427d54645c97c
shunsaku0621 <hamagi.0621@icloud.com>
2021-08-10 11:53:55 +0900

    レビューのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
-  resources :ramen_shops
+  resources :ramen_shops do
+    resources :reviews, only: [:index, :create]
+  end
 end
